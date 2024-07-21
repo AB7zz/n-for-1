0139a4af89efb253d1546b677c4fc79a30ea35bc
AB7zz <abhinavcv007@gmail.com>
2024-07-21 12:41:52 +0530

    connec to wallet

--- packages/frontend/src/components/Home/Navbar.jsx
+++ packages/frontend/src/components/Home/Navbar.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
+import { ethers } from 'ethers';
 
 const Navbar = () => {
+    const [isConnected, setIsConnected] = React.useState(false);
+    const [account, setAccount] = React.useState(null);
+
+    const connectToMetaMask = async () => {
+        try {
+        if (typeof window.ethereum !== 'undefined') {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setAccount(accounts[0]);
+            setIsConnected(true);
+        } else {
+            console.log("Please install MetaMask!");
+        }
+        } catch (error) {
+            console.error("Error connecting to MetaMask:", error);
+        }
+    };
+
+    React.useEffect(() => {
+        const checkMetaMask = async () => {
+            if (typeof window.ethereum !== 'undefined') {
+                const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+                if (accounts.length > 0) {
+                    setAccount(accounts[0]);
+                    setIsConnected(true);
+                }
+            }
+        };
+
+        checkMetaMask();
+    }, []);
   return (
     <nav className="fixed w-full bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
         <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
@@ -13,23 +44,13 @@ const Navbar = () => {
                         d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
                 </svg>
             </div>
-            <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
-                <ul className="flex font-semibold justify-between">
-                    
-                    {/* <li className="md:px-4 md:py-2 text-pink-500"><a href="#">Dashboard</a></li>
-                    <li className="md:px-4 md:py-2 hover:text-pink-400"><a href="#">Search</a></li>
-                    <li className="md:px-4 md:py-2 hover:text-pink-400"><a href="#">Explore</a></li>
-                    <li className="md:px-4 md:py-2 hover:text-pink-400"><a href="#">About</a></li>
-                    <li className="md:px-4 md:py-2 hover:text-pink-400"><a href="#">Contact</a></li> */}
-                </ul>
-            </div>
             <div className="order-2 md:order-3">
-                <button className="px-4 py-2 bg-pink-500 hover:bg-pink-600 text-gray-50 rounded-xl flex items-center gap-2">
-                  
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
-                    </svg> */}
-                    <span>Connect</span>
+                <button
+                    className={`px-4 py-2 bg-pink-500 hover:bg-pink-600 text-gray-50 rounded-xl flex items-center gap-2 ${isConnected ? 'cursor-not-allowed opacity-50' : ''}`}
+                    onClick={isConnected ? () => {} : connectToMetaMask} // Disable button on connect
+                    disabled={isConnected} // Alternatively, use disabled prop
+                >
+                    <span>{isConnected ? `${account.slice(0, 10)}...` : 'Connect'}</span>
                 </button>
             </div>
         </div>
